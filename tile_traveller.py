8538eb509f39b8e90460db4b182b823e888d3068
Stella Dögg Blöndal <StellaDogg@Stellas-MacBook-Air-2.local>
2018-09-18 10:54:28 +0000

    Implementation 1_passed

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,59 +1,83 @@
 location = 11
-
+invalidInput = False
 while location != 31:
     if location == 11 or location == 21 or location == 31:
-        print('You can travel: (N)orth.')
+        if invalidInput == False:
+            print('You can travel: (N)orth.')
         dire = input('Direction: ')
         if dire == 'n' or dire == 'N':
+            invalidInput = False
             location += 1
         else:
+            invalidInput = True
             print('Not a valid direction!')
     elif location == 12:
-        print('You can travel: (N)orth or (E)ast or (S)outh.')
+        if invalidInput == False:
+            print('You can travel: (N)orth or (E)ast or (S)outh.')
         dire = input('Direction: ')
         if dire == 'n' or dire == 'N':
+            invalidInput = False
             location += 1
         elif dire == 'e' or dire == 'E':
+            invalidInput = False
             location += 10
         elif dire == 's' or dire == 'S':
+            invalidInput = False
             location -= 1
-        else: 
+        else:
+            invalidInput = True
             print('Not a valid direction!')
     elif location == 13:
-        print('You can travel: (E)ast or (S)outh.')
+        if invalidInput == False:
+            print('You can travel: (E)ast or (S)outh.')
         dire = input('Direction: ')
         if dire == 'e' or dire == 'E':
+            invalidInput = False
             location += 10
         elif dire == 's' or dire == 'S':
+            invalidInput = False
             location -= 1
         else:
+            invalidInput = True
             print('Not a valid direction!')
     elif location == 23:
-        print('You can travel: (E)ast or (W)est.')
+        if invalidInput == False:
+            print('You can travel: (E)ast or (W)est.')
         dire = input('Direction: ')
         if dire == 'e' or dire == 'E':
+            invalidInput = False
             location += 10
         elif dire == 'w' or dire == 'W':
+            invalidInput = False
             location -= 10
         else:
+            invalidInput = True
             print('Not a valid direction!')
     elif location == 22 or location == 33:
-        print('You can travel: (S)outh or (W)est.')
+        if invalidInput == False:
+            print('You can travel: (S)outh or (W)est.')
         dire = input('Direction: ')
         if dire == 's' or dire == 'S':
+            invalidInput = False
             location -= 1
         elif dire == 'w' or dire == 'W':
+            invalidInput = False
             location -= 10
         else:
+            invalidInput = True
             print('Not a valid direction!')
     else: # location = 32
-        print('You can travel: (N)orth or (S)outh.')
+        if invalidInput == False:
+            print('You can travel: (N)orth or (S)outh.')
         dire = input('Direction: ')
         if dire == 's' or dire == 'S':
+            invalidInput = False
             location -= 1
         elif dire == 'n' or dire == 'N':
+            invalidInput = False
             location += 1
         else:
+            invalidInput = True
             print('Not a valid direction!')
 print('Victory!')
 
